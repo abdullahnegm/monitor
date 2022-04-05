2f6a49b918876cbac6f45df19e63949c1b657d60
abdullahnegm <69604740+abdullahnegm@users.noreply.github.com>
2022-04-05 09:41:02 +0200

    Update targets.js

--- controllers/targets.js
+++ controllers/targets.js
@@ -27,34 +27,37 @@ const createTargets = async (req, res) => {
 }
 
 const getReport = async(req, res) => {
-    let target = await Target.find({name: req.params.targetName},"total_uptime total_downtime uptime downtime").exec()
-    if(target.length){
-        let reqFreqs    = [target[0].uptime.length, target[0].downtime.length],
-        totalReqs       = reqFreqs[0]+reqFreqs[1],
-        aveResponse     = (target[0]?.total_uptime + target[0]?.total_downtime)/totalReqs,
-        reqPer          = (reqFreqs[0]/(totalReqs))*100,
-        reportData      = {
-            totalRequets: totalReqs,
-            total_uptime: target[0]?.total_uptime,
-            total_downtime: target[0]?.total_downtime,
-            requestPercentage:`${reqPer.toFixed(2)}%`,
-            averageResponseTime:aveResponse,
-            numberOfUptimeRequests:reqFreqs[0],
-            numberOfDowntimeRequests:reqFreqs[1],
-            uptimeRequests:target[0]?.uptime,
-            downtimeRequests:target[0]?.downtime
+    try {
+        let target = await Target.find({name: req.params.targetName},"total_uptime total_downtime uptime downtime").exec()
+        if(target.length){
+            let reqFreqs    = [target[0].uptime.length, target[0].downtime.length],
+            totalReqs       = reqFreqs[0]+reqFreqs[1],
+            aveResponse     = (target[0]?.total_uptime + target[0]?.total_downtime)/totalReqs,
+            reqPer          = (reqFreqs[0]/(totalReqs))*100,
+            reportData      = {
+                totalRequets: totalReqs,
+                total_uptime: target[0]?.total_uptime,
+                total_downtime: target[0]?.total_downtime,
+                requestPercentage:`${reqPer.toFixed(2)}%`,
+                averageResponseTime:aveResponse,
+                numberOfUptimeRequests:reqFreqs[0],
+                numberOfDowntimeRequests:reqFreqs[1],
+                uptimeRequests:target[0]?.uptime,
+                downtimeRequests:target[0]?.downtime
+            }
+            sendMail({ to: `${req.user.email}`,  subject:"monitor report", text:"monitor report", 
+            html: `
+            <h1>result of monitoring</h1>
+            <br>
+            ${json2html.render(reportData)}
+            `
+            })
+            res.status(200).send(reportData)
         }
-        sendMail({ to: `${req.user.email}`,  subject:"monitor report", text:"monitor report", 
-        html: `
-        <h1>result of monitoring</h1>
-        <br>
-        ${json2html.render(reportData)}
-        `
-        })
-        res.status(200).send(reportData)
+        return res.status(404).send("this target not found!...")
+    } catch (error) {
+        return res.status(500).send("Internal Server Error")
     }
-    
-    // res.status(200).send("this target not found!...")
 }
 
 module.exports = {getTargets, createTargets,getReport}
